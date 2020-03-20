d092a4e07dbf6c9e183c686677cc8c70639dedfd
tommylettieri <tommylettieri@gmail.com>
2020-03-20 12:45:04 -0400

    added a name form

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = {
+}
+
+// type ChangeEvent = {
+//   title: string,
+//   target: {
+//     value: string
+//   }
+// }
+
+type AppState = {
+  value: string
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    console.log("TOMTOM")
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event: any) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
 }
 
 export default App;
